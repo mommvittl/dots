d8bb5e16ee64041361685eaba7de91d9b87fd058
Misha Baranov <mishabaranov87@gmail.com>
2017-01-12 23:08:28 +0200

    Deleted basic

--- basic/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=dots',
-    'username' => 'dots',
-    'password' => 'dots12345',
-    'charset' => 'utf8',
-];
