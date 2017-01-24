2358f17e7380586805b8d91ee3fb34cf0d1df106
AntonGuzenko <guzik8989@gmail.com>
2017-01-24 19:44:27 +0300

    update db user

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=dots',
-    'username' => 'root',
-    'password' => '',
+    'username' => 'dots',
+    'password' => 'dots12345',
     'charset' => 'utf8',
 ];
