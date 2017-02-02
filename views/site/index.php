211650de2acf34bbbd9dc350a66c17cb9fd5bf37
Misha Baranov <mishabaranov87@gmail.com>
2017-02-02 22:11:56 +0200

    remove var_dump

--- views/site/index.php
+++ views/site/index.php
@@ -6,8 +6,6 @@ $this->title = 'Dots';
 $this->registerJsFile('https://unpkg.com/leaflet@1.0.2/dist/leaflet.js', ['position' => yii\web\View::POS_HEAD]);
 $this->registerCssFile('https://unpkg.com/leaflet@1.0.2/dist/leaflet.css');
 $this->registerJsFile('/web/js/game.js', ['position' => yii\web\View::POS_END]);
-//var_dump($_SESSION);
-var_dump($this->jsFiles);
 
 ?>
 <div class="site-index">
